987157fc42950064f02eb675ab07cca223813eaa
Ben Cooper <cooperben83@gmail.com>
2021-11-04 18:35:08 -0400

    updated link to project button

--- components/buttons/ExternalLinkButton.tsx
+++ components/buttons/ExternalLinkButton.tsx
@@ -3,9 +3,32 @@ import styled from "styled-components";
 const StyledButton = styled.a`
   display: flex;
   flex-direction: row;
-  align-items: flex-start;
-  padding: 10px;
+  justify-content: center;
+  align-items: center;
+  padding: 4px 10px;
+  background: #ffffff;
+  border: none;
+
+  /* Card Shadow */
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 6px;
+
   cursor: pointer;
+
+  &:hover {
+    box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+const Title = styled.p`
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  text-decoration: none;
+  margin-left: 6px;
+  margin-right: 6px;
+  color: ${(props) => props.theme.text};
 `;
 
 interface ButtonProps {
@@ -14,6 +37,7 @@ interface ButtonProps {
 export const ExternalSiteButton = ({ link }: ButtonProps) => {
   return (
     <StyledButton href={link} target="_blank">
+      <Title>Link to Project</Title>
       <img src="/svg/External.svg" alt="external Icon" />
     </StyledButton>
   );
