cab5707097b7247482f152b320b2f7a5102ab59e
Ben Cooper <cooperben83@gmail.com>
2022-03-27 13:16:44 -0400

    added github link to footer

--- components/Footer/SocialLinks.tsx
+++ components/Footer/SocialLinks.tsx
@@ -1,35 +1,39 @@
 import styled from "styled-components";
 import {
-  FaDribbble,
-  FaInstagram,
-  FaLinkedinIn,
-  FaTwitter,
+	FaDribbble,
+	FaGithub,
+	FaInstagram,
+	FaLinkedinIn,
+	FaTwitter,
 } from "react-icons/fa";
 
 const Row = styled.div`
-  width: 100px;
-  margin: 15px 4px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+	width: 100px;
+	margin: 15px 4px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 export const SocialLinks = () => {
-  return (
-    <Row>
-      <a href="https://www.linkedin.com/in/ben-a-cooper/">
-        <FaLinkedinIn color="white" />
-      </a>
-      <a href="https://www.instagram.com/ben.coo.per/">
-        <FaInstagram color="white" />
-      </a>
-      <a href="https://dribbble.com/ben-cooper/about">
-        <FaDribbble color="white" />
-      </a>
-      <a href="https://twitter.com/cooperben83">
-        <FaTwitter color="white" />
-      </a>
-    </Row>
-  );
+	return (
+		<Row>
+			<a href="https://www.linkedin.com/in/ben-a-cooper/">
+				<FaLinkedinIn color="white" />
+			</a>
+			<a href="https://github.com/bencooper96">
+				<FaGithub color="white" />
+			</a>
+			<a href="https://www.instagram.com/ben.coo.per/">
+				<FaInstagram color="white" />
+			</a>
+			<a href="https://dribbble.com/ben-cooper/about">
+				<FaDribbble color="white" />
+			</a>
+			<a href="https://twitter.com/cooperben83">
+				<FaTwitter color="white" />
+			</a>
+		</Row>
+	);
 };
